41e6899c2be26ab01285d054c47eb7dc32765fae
Mats Punt <mats.punt@live.nl>
2023-06-16 15:31:12 +0300

    Rewrote XYZ to GRO parser, now estimates box.
    Included 'tissue.py' for custom geometries.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from packmol import PackmolExecuterSingleCell
 from gromacs import Gromacs_IO
 
 """
-    Currently (29/05), there are some redundant calls to the parser classes. Let's monitor how much that slows down the execution, if this is insignificant
+    Currently (06/06), there are some redundant calls to the parser classes. Let's monitor how much that slows down the execution, if this is insignificant
     I prefer leaving it like this as it makes everything modular (i.e. GMX and PACKMOL classes are totally separated)
     If we want only a single call to the parser functions, I need to rethink how I inherit parsing functionality. Let's see...
     Using 'numba' in code to speedup? I don't really use numpy that much, since 'genfromtxt()' or 'loadtxt()' are quite slow. 
@@ -18,5 +18,5 @@ GMX.convert_xyz_to_gro("CELL.xyz", "CELL.gro")
 GMX.build_GMX_top_single_CELL("CELL.gro")
 
 
-#Note for Mats: to package this as a Python package, see: https://packaging.python.org/en/latest/tutorials/packaging-projects/
+#Note: to package this as a Python package, see: https://packaging.python.org/en/latest/tutorials/packaging-projects/
 
